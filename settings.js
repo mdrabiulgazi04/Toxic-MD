ceefe51245d6e1128229fd8b7f5cfb0298bf9b93
mdrabiulgazi04 <mdrabiulgazi86@gmail.com>
2025-11-03 03:13:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348024906596"
+global.ownernomer = "918972428638"
 global.ownername = "🃏Joker🃏"
 global.ytname = "YT: Resonex"
 global.socialm = "GitHub: Joker-Reincarnated"
