08668de2aae06a41ea133a44258962379feb70c5
mdrabiulgazi04 <mdrabiulgazi86@gmail.com>
2025-11-03 03:11:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.ytname = "YT: Resonex"
 global.socialm = "GitHub: Joker-Reincarnated"
 global.location = "Nigeria, Lagos, Ikeja"
 
-global.ownernumber = '2349168929037'  //creator number
+global.ownernumber = '918972428638'  //creator number
 global.ownername = '🃏Joker🃏' //owner name
 global.botname = '☣️ Toxic MD' //name of the bot
 
